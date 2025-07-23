95d781048239685ed8c652411c6c61749105547d
everviera12 <evertigre10@gmail.com>
2025-07-23 10:15:00 -0600

    update: github link added

--- src/App.tsx
+++ src/App.tsx
@@ -67,7 +67,13 @@ function App() {
 
     return (
         <div className='space-y-6'>
-            <div className='p-2 flex gap-4 justify-end'>
+            <div className='p-2 flex gap-4 items-center justify-end'>
+                <a href="https://github.com/everviera12/crypto-dashboard" target="_blank" rel="noreferrer">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="size-6" viewBox="0 0 16 16">
+                        <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8" />
+                    </svg>
+                </a>
+
                 <button onClick={toggleModal}>
                     <Icon className='size-6' name='heart' />
                 </button>
@@ -148,7 +154,7 @@ function App() {
             ) :
                 <div className="flex items-center justify-center h-64">
                     <span className="text-red-400 text-xl font-semibold drop-shadow-[0_0_6px_rgba(255,0,0,0.6)]">
-                        Data not available
+                        Data not
                     </span>
                 </div>}
 
