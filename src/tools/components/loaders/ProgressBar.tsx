0a92b416e612161f5d392750d35a49871821dc08
everviera12 <evertigre10@gmail.com>
2025-08-20 16:39:21 -0600

    only styles

---
+++ src/tools/components/loaders/ProgressBar.tsx
@@ -0,0 +1,7 @@
+export default function ProgressBar() {
+    return (
+        <div className='flex justify-center'>
+            <div className="loader w-1/2 h-5 rounded-full" />
+        </div>
+    );
+}
